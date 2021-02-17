ec7a262cbc85072bc81902751bb28572501ca139
GeorgeMalandris <georgemalandris@gmail.com>
2021-02-17 13:09:24 +0200

    removed the test database from the solution

--- src/app/connectionStrings.ts
+++ src/app/connectionStrings.ts
@@ -1,5 +1,5 @@
 export enum connectionStrings{
-    "https://capstonedb-a452b-default-rtdb.firebaseio.com/",
+    //"https://capstonedb-a452b-default-rtdb.firebaseio.com/",
     "https://capstonedb-act-default-rtdb.firebaseio.com/" = 1,
     "https://capstonedb-dataviva-default-rtdb.firebaseio.com/",
     "https://capstonedb-onelity-default-rtdb.firebaseio.com/"
